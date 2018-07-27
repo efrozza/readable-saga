c144423bb4d1611ad87fc84a4701313323db4cfe
Everton <evertonfrozza@gmail.com>
2018-07-27 08:00:29 -0400

    'server'

--- api-server/categories.js
+++ api-server/categories.js
@@ -1,24 +1,39 @@
-const categories = [
-  {
-    name: 'react',
-    path: 'react'
-  },
-  {
-    name: 'redux',
-    path: 'redux'
-  },
-  {
-    name: 'udacity',
-    path: 'udacity'
-  }
-]
+const clone = require('clone')
+const config = require('./config')
 
-function getData () {
-  return categories
+let db = {}
+
+const defaultData = {
+  categories: [
+    {
+      name: 'react',
+      path: 'react'
+    },
+    {
+      name: 'redux',
+      path: 'redux'
+    },
+    {
+      name: 'udacity',
+      path: 'udacity'
+    }
+  ]
 }
 
-function getAll () {
-  return new Promise(res => res(getData()))
+function getData (token) {
+  // Each token has it's own copy of the DB. The token in this case is like an app id.
+  let data = db[token]
+  // This populates the default user data if there isn't any in the db.
+  if (data == null) {
+    data = db[token] = clone(defaultData)
+  }
+  return data
+}
+
+function getAll (token) {
+  return new Promise(res => {
+    res(getData(token))
+  })
 }
 
 module.exports = {
